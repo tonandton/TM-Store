3eb6934c375968fa148d0edc6c9848ea7471a393
tonandton <www.sorasakok@gmail.com>
2025-02-14 23:14:51 +0700

    update Productvariation

--- app/Filament/Resources/ProductResource/Pages/ProductVariations.php
+++ app/Filament/Resources/ProductResource/Pages/ProductVariations.php
@@ -41,4 +41,68 @@ class ProductVariations extends EditRecord
 
               return $data;
        }
+
+       private function mergeCartesianWithExisting($variationTypes, $existingData): array
+       {
+              $defaultQuanTity = $this->record->quantity;
+              $defaultPrice = $this->record->price;
+              $cartesianProduct = $this->cartesianProduct($variationTypes, $defaultQuanTity);
+              $mergedResult = [];
+
+              foreach ($cartesianProduct as $product) {
+                     // Extract option IDs from the currect product combination as an array
+
+                     $optionIds = collect($product)
+                            ->filter(fn($value, $key) => str_starts_with($key, 'variation_type_'))
+                            ->map(fn($option) => $option['id'])
+                            ->values()
+                            ->toArray();
+
+                     // Find matching entry in existing data
+                     $match = array_filter($existingData, function ($existingOption) use ($optionIds) {
+                            return $existingOption['variation_type_option_ids'] === $optionIds;
+                     });
+
+                     // If match found overide quantity and price
+                     if (!empty($match)) {
+                            $existingEntry = reset($match);
+                            $product['quantity'] = $existingEntry['quantity'];
+                            $product['price'] = $existingEntry['price'];
+                     } else {
+                            // Set default quantity and price if no match
+                            $product['quantity'] = $defaultQuanTity;
+                            $product['price'] = $defaultPrice;
+                     }
+
+                     $mergedResult[] = $product;
+              }
+
+              return $mergedResult;
+       }
+
+       private function cartesianProduct($variationTypes, $defaultQuanTity = null): array
+       {
+              $result = [[]];
+
+              foreach ($variationTypes as $index => $variationType) {
+                     $temp = [];
+
+                     foreach ($variationType->options as $option) {
+                            // Add the current option to all existing combination
+                            foreach ($result as $combination) {
+                                   $newCombination = $combination + [
+                                          'variation_type_' . ($variationType->id) => [
+                                                 'id' => $option->id,
+                                                 'name' => $option->name,
+                                                 'label' => $variationType->name,
+                                          ],
+                                   ];
+
+                                   $temp[] = $newCombination;
+                            }
+                     }
+
+                     $result = $temp; // Update results with the new combination 
+              }
+       }
 }
